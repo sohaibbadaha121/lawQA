7c19378e0bee45b063f61bebd730998ebcd4f7fb
Sohaib badaha <1210472@student.birzeit.edu>
2025-11-05 11:56:30 +0000

    craete the DTOs

---
+++ lawQA/src/main/java/com/lawQA/lawQA/DTOs/LawRequest.java
@@ -0,0 +1,9 @@
+package com.lawQA.lawQA.DTOs;
+
+import lombok.Data;
+
+@Data
+public class LawRequest {
+    private String lawText;
+    private String question;
+}
---
+++ lawQA/src/main/java/com/lawQA/lawQA/DTOs/LawResponse.java
@@ -0,0 +1,10 @@
+package com.lawQA.lawQA.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LawResponse {
+    String answer;
+}
