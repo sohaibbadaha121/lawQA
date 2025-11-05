4fcbb283b2ba097876332701c77366210554d0cb
Sohaib badaha <1210472@student.birzeit.edu>
2025-11-05 11:50:46 +0000

    Initial commit

---
+++ lawQA/src/main/java/com/lawQA/lawQA/LawQaApplication.java
@@ -0,0 +1,12 @@
+package com.lawQA.lawQA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawQaApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(LawQaApplication.class, args);
+	}
+}
+
---
+++ lawQA/src/test/java/com/lawQA/lawQA/LawQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawQA.lawQA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
